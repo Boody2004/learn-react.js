7c2a31f47cb8dd265a80cb70e5c301c94a53b83e
Abd Elrahman Atef <65117957+Boody2004@users.noreply.github.com>
2022-02-18 01:06:43 +0200

    add if conditional in team

--- src/components/TeamMember.js
+++ src/components/TeamMember.js
@@ -2,18 +2,53 @@ import React from "react";
 
 function TeamMember(props) {
   return (
-    <div className="col-md-4 col-sm-6">
+    <div
+      className="col-md-4 col-sm-6"
+      style={{ display: props.name ? "block" : "none" }}
+    >
       <div className="card my-3">
         <div className="card-header">
           <img style={{ maxWidth: "100%" }} src={props.img} alt="image" />
         </div>
 
-        <div className="card-body">
-          <h3>{props.name}</h3>
-          <h5>{props.position}</h5>
-          <h5>{props.phone}</h5>
-          <h5>{props.email}</h5>
-          <h5>{props.website}</h5>
+        <div
+          className="card-body"
+          style={{
+            border:
+              props.position && props.phone && props.email && props.website
+                ? "white"
+                : "3px solid  red",
+          }}
+        >
+          <h3>Name: {props.name}</h3>
+          <h5
+            style={{
+              display: props.position ? "block" : "none",
+            }}
+          >
+            Position: {props.position}
+          </h5>
+          <h5
+            style={{
+              display: props.phone ? "block" : "none",
+            }}
+          >
+            Phone: {props.phone}
+          </h5>
+          <h5
+            style={{
+              display: props.email ? "block" : "none",
+            }}
+          >
+            Email: {props.email}
+          </h5>
+          <h5
+            style={{
+              display: props.website ? "block" : "none",
+            }}
+          >
+            Website: {props.website}
+          </h5>
         </div>
       </div>
     </div>
