138805649794486838d4a79ad2ad6e3163f54c1f
Abd Elrahman Atef <65117957+Boody2004@users.noreply.github.com>
2022-02-21 19:27:52 +0200

    add new react app with learn react.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from "react";
-import "./StyleApp.css";
-import Header from "./components/Header";
-import Text from "./components/Text";
-import Social from "./components/Social";
-import Team from "./components/Team";
-import File from "./components/File";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <div className="container">
-        <Text />
-        <Social />
-        <Team />
-        <File name="Boody" lastName="Atef" />
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
